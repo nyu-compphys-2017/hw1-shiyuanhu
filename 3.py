b4f19e8a80b9603176dcb56bd897db8479819f4b
Shiyuan Hu <sh4670@nyu.edu>
2017-09-14 00:03:17 -0400

    code for 3.8

---
+++ 3.py
@@ -0,0 +1,22 @@
+from __future__ import division
+from numpy import loadtxt
+from pylab import scatter,show, plot, linspace
+e = 1.602*pow(10,-19)
+
+data = loadtxt("millikan.txt",float)
+x = data[:,0]
+y = data[:,1]
+scatter(x,y)
+
+ex = sum(x)/len(x)
+ey = sum(y)/len(y)
+exx = sum(x*x)/len(x)
+exy = sum(x*y)/len(x)
+m = (exy-ex*ey)/(exx-ex**2)
+c = (exx*ey-ex*exy)/(exx-ex**2)
+print m*e
+
+x1 = linspace(min(x),max(x),10)
+y1 = m*x1+c
+plot(x1,y1)
+show()
